b291706f65d8b5908af528884c98464c299ffef2
moonrock <tinysrc@163.com>
2013-10-25 17:17:21 +0800

    x

--- rpc_callback.cpp
+++ rpc_callback.cpp
@@ -2,5 +2,12 @@
 
 void rpc_cb(conn *c, unsigned char *msg, size_t sz)
 {
+    msg_head h;
+    if (0 != message_head(msg, sz, &h)) {
+        return;
+    }
+
+    printf("magic:%d len:%d cmd:%d, flags:%d\n", h.magic, h.len, h.cmd, h.flags);
+
     conn_write(c, msg, sz);
 }
